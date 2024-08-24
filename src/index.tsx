c03a5eed39bfdb0c56c27fd456e7f359e556bb02
Danil <zarifullin17022005@icloud.com>
2024-08-24 14:01:57 +0500

    проект с сохранением состояния в localstorage

--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,20 @@ import { store } from './store/store'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { ModalWindow } from './components/ModalWindow/ModalWindow'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor } from './store/store'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <BrowserRouter>
   <Provider store={store} >
+  <PersistGate loading={null} persistor={persistor}>
       <React.StrictMode>
         <App />
         <ModalWindow />
       </React.StrictMode>
+    </PersistGate>
   </Provider>  
   </BrowserRouter>
   
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,19 +1,58 @@
 import { configureStore } from "@reduxjs/toolkit";
 import MoviesReducer from "./MoviesSlice"
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import {
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+
+
+
+const reducers = combineReducers({
+    movies: MoviesReducer,
+    favorities: MoviesReducer,
+    allMovies: MoviesReducer,
+    ids: MoviesReducer,
+    searchMovies: MoviesReducer,
+    value: MoviesReducer,
+    modalWindow: MoviesReducer,
+    navigationVisible: MoviesReducer,
+    themeValue: MoviesReducer
+ })
+
+ const persistConfig = { 
+    key: 'movies' , 
+    storage, 
+    whitelist: ["movies", "favorities", "allMovies", "ids", "searchMovies", "value", "modalWindow", "navigationVisible" ]
+};
+
+ const persistedReducer = persistReducer(persistConfig, reducers);
 
 
 export const store = configureStore({
-    reducer:{
-        movies: MoviesReducer,
-        favorities: MoviesReducer,
-        allMovies: MoviesReducer,
-        ids: MoviesReducer,
-        searchMovies: MoviesReducer,
-        value: MoviesReducer,
-        modalWindow: MoviesReducer,
-        navigationVisible: MoviesReducer,
-        themeValue: MoviesReducer
-    }
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                // need to add unnecessary action in a list for avoiding
+                // errors and warning
+                ignoredActions: [
+                         PERSIST,
+                         FLUSH, 
+                         REHYDRATE, 
+                         PAUSE, 
+                         PURGE,
+                         REGISTER
+                         ],
+            },
+        }),
 })
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
+export const persistor = persistStore(store)
